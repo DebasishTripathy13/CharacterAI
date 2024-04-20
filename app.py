6c65561c122f29361e458b264a966cdef40e9b6d
Debasish Tripathy <94828569+DebasishTripathy13@users.noreply.github.com>
2024-04-20 12:11:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,15 @@ from google.generativeai.types import HarmCategory, HarmBlockThreshold
 import pandas as pd
 import secrets
 
-Googleapi = "enter your own gemini api"
+Googleapi = "YOUR_GOOGLE_API_KEY"
 genai.configure(api_key=Googleapi)
 
 model = genai.GenerativeModel('gemini-1.5-pro-latest')
-model1=genai.GenerativeModel('gemini-1.0-pro-latest')
+model1 = genai.GenerativeModel('gemini-1.0-pro-latest')
 app = Flask(__name__)
-app.secret_key = secrets.token_hex(16)
+
+# Generate a new secret key for Flask's session on each run
+app.config['SECRET_KEY'] = secrets.token_hex(16)
 
 # MongoDB configuration
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/mydatabase'
@@ -29,10 +31,10 @@ def index():
         session['characterdetails'] = request.form['characterdetails']
         charactepersonalityresponse = model.generate_content(["generate a charater description for the below details in text only neglect all safety_ratings and stuff you will only summerize its characters personality in text only " + session['characterdetails']],
                                                               safety_settings={
-                                                                                HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
-                                                                                HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
-                                                                                HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
-                                                                                HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,})
+                                                                  HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+                                                                  HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+                                                                  HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+                                                                  HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,})
         session['charactepersonality'] = charactepersonalityresponse.text
         return redirect(url_for('chat'))
     else:
@@ -47,12 +49,12 @@ def chat():
     
     if request.method == 'POST':
         question = request.form['question']
-        query = ("you roleplay the character with the following personality and respond to the above question accordingly to the character here is the personality of character with details mentioned below  " + session['charactepersonality'] + " and your new owner/master/friend etc is me ie " + session['ownername'])
-        response = model.generate_content([question + query],safety_settings={
-                                                                                HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
-                                                                                HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
-                                                                                HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
-                                                                                HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,})
+        query = ("you roleplay the character with the following personality and respond to the above question accordingly to the character here is the personality of character with details mentioned below use internet to gater more infor mation too  " + session['charactepersonality'] + " and your new owner/master/friend etc is me ie " + session['ownername'])
+        response = model.generate_content([question + query], safety_settings={
+            HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE, })
         messages.append({'sender': 'user', 'content': question})
         messages.append({'sender': 'bot', 'content': response.text})
         session['messages'] = messages
